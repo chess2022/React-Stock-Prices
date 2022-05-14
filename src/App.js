a5b55c20d076611cc4f9ea8b61de87d56c088f44
chess2022 <lakeshore.hnh@gmail.com>
2022-05-14 15:05:51 -0700

    Adds basic pages and checks linked/working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main"
+import About from "./pages/About"
+import Nav from './components/Nav';
+import Stocks from "./pages/Stocks"
+import Prices from "./pages/Prices"
 
 export default function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/stocks" element={<Stocks />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks/:id" element={<Prices />} />
       </Routes>
     </div>
   );
